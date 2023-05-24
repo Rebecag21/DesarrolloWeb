a019b05ffade636d0cb5e5eec0b5251f758a10b0
Rebecag21 <119269748+Rebecag21@users.noreply.github.com>
2023-05-24 09:41:45 -0600

    Add files via upload

---
+++ Proyecto AyR/js/menu.js
@@ -0,0 +1,10 @@
+const hamburguer= document.querySelector('.hamburger');
+const hmenu= document.querySelector('.menu-navegacion');
+
+console.log(menu-navegacion)
+console.log(hamburguer)
+
+hamburguer.addEventListener('click', ()=>{
+    alert("click")
+
+})
